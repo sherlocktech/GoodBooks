486413eeb8d9dede2a3f4f62b469f911b7b14a3d
Sean <sean.stamm5@gmail.com>
2020-08-10 12:58:31 -0700

    Add GoodBooks sln

---
+++ Goodbooks.Web/Controllers/WeatherForecastController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GoodBooks.Web.Controllers
+{
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/api/books/")]
+        public ActionResult GetBooks()
+        {
+            return Ok("Books!");
+        }
+    }
+}
